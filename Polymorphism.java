276a6dd127c6bbdbd591c344b456bce22b7e51d2
BRavinder <105974685+BRavinder@users.noreply.github.com>
2023-09-20 11:24:45 +0530

    Update Polymorphism.java

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,15 +1,21 @@
+/*Polymorphism in Java is a concept by which we can perform a single action in different ways. Polymorphism is derived from 2 Greek words: poly and morphs. The word "poly" means many and "morphs" means forms. So polymorphism means many forms.
+
+There are two types of polymorphism in Java: compile-time polymorphism and runtime polymorphism. We can perform polymorphism in java by method overloading and method overriding.*/
+
+
+
 class Animal {
-    public void animalsound(){
+    public void Animalsound(){
         System.out.println("the animal make a sound..");
     }
 }
 class Pig extends Animal {
-    public void animalsound(){
+    public void Animalsound(){
         System.out.println("The pig says: we wee..");
     }
 }
 class dog extends Animal {
-    public void animalsound(){
+    public void Animalsound(){
         System.out.println("the dog says : bow wow..");
     }
 }
@@ -20,9 +26,9 @@ class Main{
         Animal myAnimal = new Animal();
         Animal myPig = new Pig();
         Animal myDog = new dog();
-        myAnimal.animalsound();
-        myPig.animalsound();
-        myDog.animalsound();
+        myAnimal.Animalsound();
+        myPig.Animalsound();
+        myDog.Animalsound();
 
 
     }
